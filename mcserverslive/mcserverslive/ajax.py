86df9fde96267cb6991ccbb9d84cef83a5227cc7
kevinpw <kevin@world-sales.net>
2014-08-08 16:01:07 -0400

    switch plot var

--- mcserverslive/mcserverslive/ajax.py
+++ mcserverslive/mcserverslive/ajax.py
@@ -1,20 +1,21 @@
-import json
+import json, pytz
 from datetime import datetime
 from dajaxice.decorators import dajaxice_register
 
 from mcserverslive.models import Server
 
 def milli_since_epoch(dt):
-	epoch = datetime(1970,1,1)
-	dt = dt.replace(tzinfo=None)
-	delta_secs = (dt-epoch).total_seconds()
+	epoch = pytz.utc.localize(datetime(1970,1,1))
+#	dt = pytz.utc.localize(dt)
+	delta = dt-epoch
+	delta_secs = (delta).total_seconds()
 	return int(delta_secs*1000)
 
 @dajaxice_register
 def get_data(request, pk):
 
 	data = Server.objects.get(pk=pk).archivenumplayers_set.all()
-	data = { milli_since_epoch(d.query_time): d.percent_up for d in data }
+	data = { milli_since_epoch(d.query_time): d.num_players for d in data }
 	
 	return json.dumps({'data': data})
 
