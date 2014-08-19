6c2507a27b1d906796e4eec86917a69538a6325b
kevinpw <kevin@world-sales.net>
2014-08-19 11:34:09 -0400

    error

--- mcserverslive/accounts/urls.py
+++ mcserverslive/accounts/urls.py
@@ -5,5 +5,4 @@ urlpatterns = patterns('',
 
 	url(r'^register/$', CustomRegistrationView.as_view(), name='register'),
 	url(r'^email_settings/(?P<pk>\d+)/$', EmailSettingsUpdateView.as_view(), name='email_settings'),
-
 )
