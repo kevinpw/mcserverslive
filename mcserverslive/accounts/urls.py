8c5df665d6b270942d1ae333a0d55e3c6ed92754
kevinpw <kevin@world-sales.net>
2014-08-21 09:11:46 -0400

    urls and forms tweak

--- mcserverslive/accounts/urls.py
+++ mcserverslive/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 
 	url(r'^register/$', CustomRegistrationView.as_view(), name='register'),
 	url(r'^email_settings/(?P<pk>\d+)/$', EmailSettingsUpdateView.as_view(), name='email_settings'),
+	url(r'^email_settings/$', EmailSettingsUpdateView.as_view(), name='email_settings'),
 )
